77f18ee9250b414f43dfd9d8f6be53bac391c450
Tobias Moxter <tobiasmoxter@sp-eduroam-207-255.wireless.uva.nl>
2022-11-29 11:50:17 +0100

    Tabu search alternative for random walk.

--- genepro/local_search.py
+++ genepro/local_search.py
@@ -7,7 +7,7 @@ import genepro.comptree_generators as treegen
 from typing import Tuple
 
 
-class RandomWalk(Encoder):
+class RandomWalkTabu(Encoder):
     """Searches the landscape locally by making small moves in the encoded space, which
     are expected to resemble small changes with reagard to tree fitness. Moves that
     result in worse fitness are always discarded. Inherits from the Encoder to perform
@@ -21,10 +21,44 @@ class RandomWalk(Encoder):
 
     def __init__(self, unaryNodes: list, binaryNodes: list, leafNodes: list,
         train_x : np.ndarray, train_y : np.ndarray) -> None:
-        super().__init__(unaryNodes, binaryNodes, leafNodes)
+        super().__init__(unaryNodes, binaryNodes, leafNodes, train_x, train_y)
         self.train_x = train_x
         self.train_y = train_y
         self.plot = False
+        self.visited = dict()
+        self.temp_rep_count = 0
+    
+    def _valid_change(self, encoding : np.ndarray) -> bool:
+        """Reject change if the region of the search space has been visited.
+        Add region to map of visited regions if npt.
+        
+        Parameters
+        ------
+        encoding :np.ndarray: ecoded tree
+        
+        Returns
+        -------
+        :bool: false if rejected"""
+
+        arCount = [2,2,0,0,2,0,0] # -> hardcoded for depth 2 atm, will be changed
+        sequence = []
+        for i, vector in enumerate(encoding):
+            if arCount[i] == 0:
+                ids = list(np.unravel_index(
+                    vector[-len(self.leafNodes):].argmax(),
+                    vector[-len(self.leafNodes):].shape)
+                    )
+                ids[0] += self.internalNodes.shape[0] - len(self.leafNodes)
+            else:
+                ids = np.unravel_index(vector.argmax(), vector.shape)
+            sequence.append((int(ids[0]), int(ids[1])))
+        sequence = tuple(s for s in sequence)
+        if sequence in self.visited:
+            self.temp_rep_count += 1
+            return False
+        else:
+            self.visited[sequence] = True
+            return True
     
     def local_variation(self, tree : Node, step_size : float) -> Node:
         """Get new tree that is a local (in ecoding) variation of original tree.
@@ -32,16 +66,23 @@ class RandomWalk(Encoder):
         Return :Node: New tree after variation"""
         
         encoding = self.encode(tree, self.train_x)
+        variation = encoding
         shpx, shpy, shpz = encoding.shape
-        rand_angle = np.random.randn(shpx, shpy, shpz)
-        rand_angle = rand_angle / np.linalg.norm(rand_angle) * step_size
-        variation = encoding + rand_angle
+        attempts = 0
+        while not self._valid_change(variation):
+            rand_angle = np.random.randn(shpx, shpy, shpz)
+            rand_angle = rand_angle / np.linalg.norm(rand_angle) * step_size
+            variation = encoding + rand_angle
+            attempts += 1
+            if attempts > 50:
+                step_size  = int(1.25 * step_size)
+                attempts = 0
         tree = self.decode(variation)
         
         return tree
     
-    def search(self, step_size : float, n_attempts : int, restarts : int,
-                                        fitness : callable = None) -> Tuple:
+    def search(self, step_size : float, n_attempts : int = 5, restarts : int = 1,
+                                        fitness : callable = None, tree : Node = None) -> Tuple:
         """Perform search in the embedded space: perform local variation, discard
         if less fit, keep if more. Step size is uniformally sampled between 0
         and given step size. If not fitness function is provided, MSE is used.
@@ -50,7 +91,7 @@ class RandomWalk(Encoder):
         -----
         step_size :float: upper limit for the r2 distance moved in ecoded space,
         n_attempts :int: number of consecutive steps performed without an improvement,
-        restarts :int: number of restarts, fitness :callable: if provided, needs to 
+        restarts :int: number of starts at least 1, fitness :callable: if provided, needs to 
         have call singniture fitness(tree : Node).
         
         Returns
@@ -64,8 +105,7 @@ class RandomWalk(Encoder):
                 np.power(tree(self.train_x)-self.train_y, 2)
                 )
         # --- initialize ---
-        t0 = treegen.sample_tree(self.unaryNodes, self.binaryNodes,
-                                                self.leafNodes, 2)
+        t0 = tree
         best_of_runs_f = fitness(t0)
         best_of_runs_t = t0
         track = []
@@ -76,7 +116,6 @@ class RandomWalk(Encoder):
                                      -  np.min(self.train_y))/ 2 * 2.5
         # --- main loop ---
         for i in range(restarts):
-            print("(Re)start: %s..." %i)
             # --- --- (re)initialize --- ---
             sample = []
             j = 0
@@ -87,7 +126,7 @@ class RandomWalk(Encoder):
             while j < n_attempts:
                 vc += 1
                 var_t = self.local_variation(best_of_var_t,
-                        step_size=np.random.random()*step_size)
+                        step_size=step_size)
                 var_f = fitness(var_t)
 
                 # --- --- plotting --- ---
@@ -115,16 +154,11 @@ class RandomWalk(Encoder):
             if best_of_var_f > best_of_runs_f:
                 best_of_runs_t = best_of_var_t
                 best_of_runs_f = best_of_var_f
-                if best_of_runs_f > 1e-6:
-                    print("Terminating sucessfully...")
-                    break
-            t0 = treegen.sample_tree(self.unaryNodes, self.binaryNodes,
-                                                    self.leafNodes, 2)
+
         # ---
         
         if self.plot:
             self._create_gif()
-        print("Done")
         return best_of_runs_f, best_of_runs_t, track
 
     def _plot_functions(self, x, ax, sample, best):
@@ -150,7 +184,8 @@ class RandomWalk(Encoder):
             images.append(images[-1])
         imageio.mimsave('temp/movie.gif', images)
 
-class SimulatedAnnealing(Encoder):
+
+class SimulatedAnnealingTabu(Encoder):
     """Simulated annealing procedure for a single solution candidate. Local changes are made,
     of which 'good' moves are always accepted, while 'bad' moves have a chance of being accepted
     depending both on the current temperature and the loss of fitness during the move.
@@ -167,6 +202,40 @@ class SimulatedAnnealing(Encoder):
         self.train_x = train_x
         self.train_y = train_y
         self.plot, self.sa_verbose = False, False
+        self.visited = dict()
+        self.temp_rep_count = 0
+    
+    def _valid_change(self, encoding : np.ndarray) -> bool:
+        """Reject change if the region of the search space has been visited.
+        Add region to map of visited regions if npt.
+        
+        Parameters
+        ------
+        encoding :np.ndarray: ecoded tree
+        
+        Returns
+        -------
+        :bool: false if rejected"""
+
+        arCount = [2,2,0,0,2,0,0] # -> hardcoded for depth 2 atm, will be changed
+        sequence = []
+        for i, vector in enumerate(encoding):
+            if arCount[i] == 0:
+                ids = list(np.unravel_index(
+                    vector[-len(self.leafNodes):].argmax(),
+                    vector[-len(self.leafNodes):].shape)
+                    )
+                ids[0] += self.internalNodes.shape[0] - len(self.leafNodes)
+            else:
+                ids = np.unravel_index(vector.argmax(), vector.shape)
+            sequence.append((int(ids[0]), int(ids[1])))
+        sequence = tuple(s for s in sequence)
+        if sequence in self.visited:
+            self.temp_rep_count += 1
+            return False
+        else:
+            self.visited[sequence] = True
+            return True
     
     def local_variation(self, tree : Node, step_size : float) -> Node:
         """Get new tree that is a local (in ecoding) variation of original tree.
@@ -174,10 +243,17 @@ class SimulatedAnnealing(Encoder):
         Return :Node: New tree after variation"""
         
         encoding = self.encode(tree, self.train_x)
+        variation = encoding
         shpx, shpy, shpz = encoding.shape
-        rand_angle = np.random.randn(shpx, shpy, shpz)
-        rand_angle = rand_angle / np.linalg.norm(rand_angle) * step_size
-        variation = encoding + rand_angle
+        attempts = 0
+        while not self._valid_change(variation):
+            rand_angle = np.random.randn(shpx, shpy, shpz)
+            rand_angle = rand_angle / np.linalg.norm(rand_angle) * step_size
+            variation = encoding + rand_angle
+            attempts += 1
+            if attempts > 25:
+                step_size  = int(1.25 * step_size)
+                attempts = 0
         tree = self.decode(variation)
         
         return tree
@@ -226,8 +302,7 @@ class SimulatedAnnealing(Encoder):
             best_of_var_t = t0
             # --- --- random walk loop --- ---
             for _ in range(50):
-                var_t = self.local_variation(best_of_var_t,
-                        step_size=np.random.random()*step_size)
+                var_t = self.local_variation(best_of_var_t, step_size)
                 var_f = fitness(var_t)
                 if var_f < best_of_var_f:
                     track.append(var_f - best_of_var_f)
@@ -235,9 +310,9 @@ class SimulatedAnnealing(Encoder):
                 best_of_var_f = var_f
             u = np.median(track)
             if not temp0:
-                temp0 = u / np.log(0.95)
+                temp0 = u / np.log(0.15)
             if not temp_terminate:
-                temp_terminate = u / np.log(0.35)
+                temp_terminate = u / np.log(0.01)
         
         alpha = np.log(temp_terminate/temp0) / max_temp_steps + 1
         if self.sa_verbose:
@@ -302,7 +377,7 @@ class SimulatedAnnealing(Encoder):
                     ax.plot(self.train_x, self.train_y, 'g^')
                     plt.title("Restart : %s, T: %s" % (i, 
                                         np.round(temp, 5)))
-                    fig.savefig("temp/"+str(tcnt)+".png")
+                    fig.savefig("data/temp/"+str(tcnt)+".png")
                     plt.close()
                 # ---- --- temperature lowering schedule, alpha = 0.98 --- ---
                 temp = temp * alpha #np.exp(-0.75 * temp / np.std(fdev)) needs testing
@@ -333,13 +408,13 @@ class SimulatedAnnealing(Encoder):
     
     def _create_gif(self):
         """Create a video of the search process."""
-        os.makedirs("temp", exist_ok=True)
+        os.makedirs("data/temp", exist_ok=True)
         images = list()
-        pngs = sorted([x for x in os.listdir("temp/") if x.endswith(".png")],
+        pngs = sorted([x for x in os.listdir("data/temp/") if x.endswith(".png")],
                                     key=lambda x: int(x.replace(".png","")))
         for filename in pngs:
-            images.append(imageio.imread("temp/"+filename))
+            images.append(imageio.imread("data/temp/"+filename))
         # pause on last one
         for _ in range(int(0.5*len(images))):
             images.append(images[-1])
-        imageio.mimsave('temp/movie.gif', images)
+        imageio.mimsave('data/temp/movie.gif', images)
